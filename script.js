017ff7d539b732a83fc03d154cc3104fdb464097
K.Sudalaimani <sudalaimani5112007@gmail.com>
2025-06-11 10:19:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// const user = prompt("enter Your name sir");
-// const userGreetings = `Hello ${user}`;
-// console.log(userGreetings);
-
-// const userLength = alert(`Your Name contains ${user.length} letters`)
-
 const userSwappy = prompt(`Enter Your Name to swap`);
 const firstNumberMethodSlice = userSwappy.slice(0,1);
 const middleNumberMethodSlice = userSwappy.slice(1,length-1)
